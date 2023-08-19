f1032c7fe5083bd33ce33e9e2dea575c182243ec
Radu Chelaru <raduchelaru1@gmail.com>
2023-08-19 20:41:43 +0300

    better error handling

--- src/command/execute.rs
+++ src/command/execute.rs
@@ -13,15 +13,8 @@ pub fn echo(args: &[String]) -> Result<(), String> {
 }
 
 pub fn cat(args: &[String]) -> Result<(), String> {
-    let dest_path = args.get(0);
-    let from_path = args.get(1);
-
-    if dest_path.is_none() || from_path.is_none() {
-        return Err("2 paths are needed".into());
-    }
-
-    let dest_path = dest_path.unwrap();
-    let from_path = from_path.unwrap();
+    let dest_path = args.get(0).ok_or(String::from("2 paths are needed"))?;
+    let from_path = args.get(1).ok_or(String::from("2 paths are needed"))?;
 
     let contents_from =
         read_to_string(from_path).map_err(|_| String::from("Could not read from file"))?;
