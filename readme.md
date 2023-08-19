a8c85947ec10813b05666d742c31f5a82a9f9e65
Radu Chelaru <raduchelaru1@gmail.com>
2023-08-19 15:07:31 +0300

    echo command implemented

---
+++ readme.md
@@ -0,0 +1,12 @@
+#Goals
+
+    1.echo: repeats input
+    2.cat: concatenates files
+    3.ls: lists directories
+    4.find: locates files or directories
+    5.grep: matches text in files
+
+- Add extra flags to the tools to enhance their feature set
+- Use proper error handling so the programs don't end prematurely (no .unwrap() and no .expect())
+- Add multi-threading support. find and grep are good candidates for this
+- Add colorized output
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,78 @@
-fn main() {
-    println!("Hello, world!");
+use std::env::args;
+
+enum Cmd {
+    Echo,
+    Cat,
+    Ls,
+    Find,
+    Grep,
+    None,
+}
+
+trait Execute {
+    fn execute(&self, arg: &String) -> Result<(), String>;
+}
+
+impl Execute for Cmd {
+    fn execute(&self, arg: &String) -> Result<(), String> {
+        match self {
+            Cmd::Echo => {
+                println!("{}", arg);
+                Ok(())
+            }
+            Cmd::Cat => todo!(),
+            Cmd::Ls => todo!(),
+            Cmd::Find => todo!(),
+            Cmd::Grep => todo!(),
+            Cmd::None => Err(String::from("Command not found")),
+        }
+    }
+}
+
+impl From<&String> for Cmd {
+    fn from(value: &String) -> Self {
+        let name: &str = value.as_str();
+        match name {
+            "echo" => Cmd::Echo,
+            "cat" => Cmd::Cat,
+            "ls" => Cmd::Ls,
+            "find" => Cmd::Find,
+            "grep" => Cmd::Grep,
+            _ => Cmd::None,
+        }
+    }
+}
+
+struct Command {
+    name: Cmd,
+    arg: String,
+}
+
+impl Default for Command {
+    fn default() -> Self {
+        Self {
+            name: Cmd::Echo,
+            arg: String::from("Hello there"),
+        }
+    }
+}
+
+impl Command {
+    fn execute(&self) -> Result<(), String> {
+        return self.name.execute(&self.arg);
+    }
+
+    fn new(cmd_name: &String, arg: &String) -> Self {
+        Command {
+            name: Cmd::from(cmd_name),
+            arg: arg.clone(),
+        }
+    }
+}
+
+fn main() -> Result<(), String> {
+    let args: Vec<String> = args().collect();
+    let cmd_name = args.get(1).expect("Could not get fist param");
+    let cmd_args = args.get(2).expect("Could not get second param");
+    Command::new(cmd_name, cmd_args).execute()
 }
